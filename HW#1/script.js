4273208accc9741ee7a847bfa69cbed96488617a
George <batenchuk160397@gmail.com>
2024-04-19 21:46:32 +0300

    added files with finished code

---
+++ HW#1/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const firstNumber = 10;
+const secondNumber = 5;
+
+const addition = firstNumber + secondNumber;
+const subtraction = firstNumber - secondNumber;
+const multiplication = firstNumber * secondNumber;
+const division = firstNumber / secondNumber;
+const degree = firstNumber ** 4;
+
+console.log (addition);
+console.log (subtraction);
+console.log (multiplication);
+console.log (division);
+console.log (degree);
